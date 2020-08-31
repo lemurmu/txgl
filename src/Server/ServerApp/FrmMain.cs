a49a6b481bc8ba93342d961527d301f3781fe165
lemurmu <455721762@qq.com>
2020-08-31 23:37:44 +0800

    初始化提交

---
+++ src/Server/ServerApp/FrmMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServerApp
+{
+    public partial class FrmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
